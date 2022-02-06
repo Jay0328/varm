189ca678ab05a2dc5a8223341467976689a36c54
Jay <gcobs135102@gmail.com>
2022-02-06 17:28:23 +0800

    chore: use process.cwd() in clean build script

--- scripts/cleanBuild.js
+++ scripts/cleanBuild.js
@@ -1,6 +1,6 @@
 const Package = require('./package');
 const PackageBuilder = require('./packageBuilder');
 
-const pkg = new Package(process.env.PWD);
+const pkg = new Package(process.cwd());
 
 new PackageBuilder(pkg).clean();
