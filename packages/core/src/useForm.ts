2c1e54c445ef689929ce665568d5b1de79363e61
Jay Chen <gcobs135102@gmail.com>
2022-02-15 16:22:03 +0800

    fix(core): can pass ref to options of useForm

--- packages/core/src/useForm.ts
+++ packages/core/src/useForm.ts
@@ -1,14 +1,19 @@
-import { markRaw } from 'vue';
+import { markRaw, reactive } from 'vue';
+import { MaybeRefs } from '@varm/shared';
 import { FieldsOfGroup } from './field';
 import { Form, FormOptions } from './form';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+export type UseFormOptions<TFields extends FieldsOfGroup<any>> = MaybeRefs<FormOptions<TFields>>;
+
 export function useForm<TFields extends FieldsOfGroup<any>>(
   fields: TFields,
-  options: FormOptions<TFields>
+  options: UseFormOptions<TFields>
 ): Form<TFields> {
-  return markRaw(new Form(fields, options));
+  const parsedOptions = reactive(options) as FormOptions<TFields>;
+
+  return markRaw(new Form(fields, parsedOptions));
 }
 
 /* eslint-enable */
